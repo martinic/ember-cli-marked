ac828900bd332a9898c675ae6e2c3cc11d066ce3
Martin Broerse <broerse@martinic.com>
2016-11-26 06:01:51 +0100

    Switch to broccoli-stew

--- README.md
+++ README.md
@@ -45,5 +45,7 @@ Tom Dale's blog example using Ember CLI and ember-cli-marked: [broerse/ember-cli
 And of course thanks to all our wonderful contributors, [here](https://github.com/martinic/ember-cli-marked/graphs/contributors)!
 
 ## Changelog
+* **0.1.0-beta.4** - Switch to broccoli-stew
+* **0.1.0-beta.3** - Switch to the moment.js npm package
 * **0.1.0-beta.2** - Fix blueprint
 * **0.1.0-beta.1** - First Beta release
--- index.js
+++ index.js
@@ -2,22 +2,33 @@
 'use strict';
 
 var path = require('path');
-var mergeTrees = require('broccoli-merge-trees');
+var stew = require('broccoli-stew');
 
 module.exports = {
   name: 'ember-cli-marked',
 
   treeForVendor: function() {
-    var markedLib = path.join(path.dirname(require.resolve('marked')), '.');
-    var cliMarkedVendor = path.join(__dirname, 'vendor');
-    return mergeTrees([markedLib, cliMarkedVendor]);
+    var marked = stew.find(path.join(path.dirname(require.resolve('marked')), '.'), {
+      destDir: 'marked',
+      files: ['marked.js']
+    });
+
+    var shims = stew.find(__dirname + '/vendor/marked', {
+      destDir: 'marked',
+      files: ['shims.js']
+    });
+
+    return stew.find([
+      marked,
+      shims
+    ]);
   },
 
   included(app) {
     this._super.included.apply(this, arguments);
 
-    app.import('vendor/marked.js');
-    app.import('vendor/shims/marked.js', {
+    app.import('vendor/marked/marked.js');
+    app.import('vendor/marked/shims.js', {
       exports: { 'marked': [ 'default' ]}
     });
   }
